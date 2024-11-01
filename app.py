7984e7b71ff5fef83f44f22f304fcf85f209ba17
fsiciliano21 <fsiciliano@fi.uba.ar>
2024-11-01 20:32:48 -0300

    Crear firma de funciones con sus respectivos endpoints

--- app.py
+++ app.py
@@ -4,6 +4,20 @@ from flask import Flask, request, render_template, url_for
 app: Flask = Flask(__name__)
 
 
+@app.route("/")
+def home() -> str:
+    pass
+
+
+@app.route("")
+def contact() -> str:
+    pass
+
+
+@app.route("")
+def about_us() -> str:
+    pass
+
 
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=5000, debug=True)
