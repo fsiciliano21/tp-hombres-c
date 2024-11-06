7f15aa19d8584897fe8b3efd4faa51f289c18601
Sofía_Belén_Noriega_Nuti <snoriega@fi.uba.ar>
2024-11-06 00:46:15 -0300

    Modificación de nombre de archivos, orden de las estructuras y url_for

--- app.py
+++ app.py
@@ -1,18 +1,14 @@
 from flask import Flask, request, render_template, url_for
 
-
 app: Flask = Flask(__name__)
-
 
 @app.route("/")
 def home():
     return render_template("home.html")
 
-
 @app.route("/contact")
 def contact():
     return render_template("contact.html")
-
 
 @app.route("/about_us")
 def about_us():
