ad707083d84ecad7f17c308391d15e07a3a4ead2
Benjacrack55 <bmoyano@fi.uba.ar>
2024-11-12 11:48:19 -0300

    Modificaciones para el about_us 	modified:   app/app.py 	modified:   app/static/styles/styles.css 	modified:   app/templates/about_us.html

--- app/app.py
+++ app/app.py
@@ -27,7 +27,16 @@ def contact():
 
 @app.route("/about_us")
 def about_us():
-    return render_template("about_us.html")
+    integrantes = [
+        {"apellido":"Gonzales", "nombre":"Gabriel Leonel", "padron":"112607"},
+        {"apellido":"Noriega Nuti", "nombre":"Sofia belen", "padron":"110502"},
+        {"apellido":"Rojas Calizaya", "nombre":"Kevin Edgardo", "padron":"109948"},
+        {"apellido":"Julian Gilio", "nombre":"Agustin Nahuel", "padron":"111876"},
+        {"apellido":"Siciliano", "nombre":"Fransisco", "padron":"108157"},
+        {"apellido":"Lopez", "nombre":"Lazaro", "padron":"111312"},
+        {"apellido":"Moyano", "nombre":"Benjamin", "padron":"111613"},
+    ]
+    return render_template("about_us.html", integrantes=integrantes)
 
 """ @app.route("/liyue")
 def liyue():
