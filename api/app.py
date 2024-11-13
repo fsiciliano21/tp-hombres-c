edc43507d4ee793045b598647f34ccd4b00af1b7
Benjamin <146848539+Benjacrack55@users.noreply.github.com>
2024-11-13 16:05:25 -0300

    Update app.py

--- api/app.py
+++ api/app.py
@@ -13,22 +13,34 @@ def get_all_personajes():
 
   response = []
   for row in result:
-    response.append({'ID': id[0], 'nombre': row[1], 'edad': row[2], 'region': row[3], 'elemento': row[4]})
+    response.append({
+      'ID': id[0],
+      'nombre': row[1],
+      'edad': row[2],
+      'region': row[3],
+      'elemento': row[4]
+    })
 
   return jsonify(response), 200
 
 @app.route('/api/v1/personajes/<int:id>', methods=['GET'])
-def get_by_id(id):
+def get_by_id(personaje_id):
     try:
-        result = personaje.personaje_by_id(id)
+        result = personaje.personaje_by_id(personaje_id)
     except Exception as e:
         return jsonify({'error': str(e)}), 500
 
-    if len(result) == 0:
+    #if len(result) == 0:
+    if not result:
         return jsonify({'error': 'No se encontró el personaje'}), 404
 
     result = result[0]
-    return jsonify({'nombre': row[0], 'edad': row[1], 'region': row[2], 'elemento': row[3]}), 200
+    return jsonify({
+      'nombre': row[0],
+      'edad': row[1],
+      'region': row[2],
+      'elemento': row[3]
+    }), 200
 
 
 if __name__ == "__main__":
