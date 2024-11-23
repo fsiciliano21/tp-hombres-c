2a8f7a6fe92e5485384302e7c2d9636801cfa1f5
fsiciliano21 <fsiciliano@fi.uba.ar>
2024-11-23 17:13:25 -0300

    Corregir la funcion de mostrar_alerta

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -1,11 +1,5 @@
-const act1 = document.getElementById("prox_1");
-const act2 = document.getElementById("prox_2");
-const act3 = document.getElementById("prox_3");
-window.onload = () =>{
-    act1.addEventListener('click', prox);
-    act2.addEventListener('click', prox);
-    act3.addEventListener('click', prox);
-}
-function prox() {
-    alert("proximamente");
+function mostrar_alerta(nombre_nacion) {
+    window.onload = () => {
+        alert(`${nombre_nacion}, proximamente...`);
+    };
 }
