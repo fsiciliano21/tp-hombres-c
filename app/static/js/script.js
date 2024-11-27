e5ed22d93730f850d24e1bdf2953b1b467f11bc3
fsiciliano21 <fsiciliano@fi.uba.ar>
2024-11-27 19:12:15 -0300

    Crear funcion redireccionar

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -3,3 +3,7 @@ function mostrar_alerta(nombre_nacion) {
         alert(`${nombre_nacion}, proximamente...`);
     };
 }
+
+function redireccionar(nombre_ruta, espera_explicita = 1000) {
+    setTimeout(() => {window.location.href = nombre_ruta;}, espera_explicita);
+}
