9ac4ede406cbd3b9f0d74cf918b341cbeb63d1e7
kryss <krojas@fi.uba.ar>
2024-11-27 04:38:26 -0300

    Agregar nombres a los mapas en MapaInteractivoView y mejorar la presentación con un overlay de texto.

--- aplicacion/lib/presentation/view/mapa_view.dart
+++ aplicacion/lib/presentation/view/mapa_view.dart
@@ -30,12 +30,12 @@ class _MapaInteractivoViewState extends State<MapaInteractivoView> {
         itemCount: 6,
         itemBuilder: (context, index) {
           final List<Map<String, dynamic>> mapas = [
-            {'image': 'assets/images/mapas/mondstadt.jpeg', 'screen': Mondstadt()},
-            {'image': 'assets/images/mapas/liyue.jpg', 'screen': Liyue()},
-            {'image': 'assets/images/mapas/inazuma.jpg', 'screen': Inazuma()},
-            {'image': 'assets/images/mapas/sumeru.jpeg', 'screen': Sumeru()},
-            {'image': 'assets/images/mapas/fonteine.jpg', 'screen': Fontaine()},
-            {'image': 'assets/images/mapas/natlan.png', 'screen': Natlan()},
+            {'image': 'assets/images/mapas/mondstadt.jpeg', 'screen': Mondstadt(), 'name': 'Mondstadt'},
+            {'image': 'assets/images/mapas/liyue.jpg', 'screen': Liyue(), 'name': 'Liyue'},
+            {'image': 'assets/images/mapas/inazuma.jpg', 'screen': Inazuma(), 'name': 'Inazuma'},
+            {'image': 'assets/images/mapas/sumeru.jpeg', 'screen': Sumeru(), 'name': 'Sumeru'},
+            {'image': 'assets/images/mapas/fonteine.jpg', 'screen': Fontaine(), 'name': 'Fontaine'},
+            {'image': 'assets/images/mapas/natlan.png', 'screen': Natlan(), 'name': 'Natlan'},
           ];
 
           return GestureDetector(
@@ -45,9 +45,30 @@ class _MapaInteractivoViewState extends State<MapaInteractivoView> {
                 MaterialPageRoute(builder: (context) => mapas[index]['screen']),
               );
             },
-            child: Image.asset(
-              mapas[index]['image'],
-              fit: BoxFit.cover,
+            child: Stack(
+              children: [
+                Image.asset(
+                  mapas[index]['image'],
+                  fit: BoxFit.cover,
+                  width: double.infinity,
+                  height: double.infinity,
+                ),
+                Positioned(
+                  bottom: 10,
+                  left: 10,
+                  child: Container(
+                    color: Colors.black54,
+                    padding: EdgeInsets.all(5),
+                    child: Text(
+                      mapas[index]['name'],
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 20,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
             ),
           );
         },
