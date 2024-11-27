2d0676f61b7f54671fc7e05131434d7e19e46593
Sofía_Belén_Noriega_Nuti <snoriega@fi.uba.ar>
2024-11-27 21:36:40 +0000

    Proximamente

--- aplicacion/lib/presentation/screens/mapas/inazuma.dart
+++ aplicacion/lib/presentation/screens/mapas/inazuma.dart
@@ -16,7 +16,32 @@ class _InazumaState extends State<Inazuma> {
         title: const Text('Inazuma'),
         backgroundColor: const Color(0xFFEDD9B7),
       ),
-
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Container(
+            decoration: BoxDecoration(
+              color: const Color(0xFFEDD9B7), // Color de fondo del contenedor
+              borderRadius: BorderRadius.circular(10), // Bordes redondeados
+              border: Border.all(
+                color: Colors.black, // Color del borde
+                width: 2, // Grosor del borde
+              ),
+            ),
+            padding: const EdgeInsets.all(20.0), // Espacio interno del contenedor
+            child: Text(
+              '¡Próximamente!\n\n'
+              'Estamos trabajando en este contenido.',
+              style: const TextStyle(
+                fontSize: 18,
+                fontFamily: 'Cursive', // Tipografía cursiva
+                color: Colors.black, // Color de la letra
+              ),
+              textAlign: TextAlign.center,
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
