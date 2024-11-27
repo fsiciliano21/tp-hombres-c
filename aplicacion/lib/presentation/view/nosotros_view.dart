a6619e46244f080816e2353ac497945acbc8ea15
kryss <krojas@fi.uba.ar>
2024-11-27 16:25:52 -0300

    Actualizar NosotrosView: agregar lista de integrantes con nombre y padrón, y mejorar la presentación con ListView.

--- aplicacion/lib/presentation/view/nosotros_view.dart
+++ aplicacion/lib/presentation/view/nosotros_view.dart
@@ -8,16 +8,34 @@ class NosotrosView extends StatefulWidget {
 }
 
 class _NosotrosViewState extends State<NosotrosView> {
-
+  final List<Map<String, String>> integrantes = [
+    {"apellido": "Gonzales", "nombre": "Gabriel Leonel", "padron": "112607"},
+    {"apellido": "Noriega Nuti", "nombre": "Sofia Belen", "padron": "110502"},
+    {"apellido": "Rojas Calizaya", "nombre": "Kevin Edgardo", "padron": "109948"},
+    {"apellido": "Julian Gilio", "nombre": "Agustin Nahuel", "padron": "111876"},
+    {"apellido": "Siciliano", "nombre": "Franco", "padron": "108157"},
+    {"apellido": "Lopez", "nombre": "Lazaro", "padron": "111312"},
+    {"apellido": "Moyano", "nombre": "Benjamin", "padron": "111613"},
+  ];
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-
-        ],
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Nosotros'),
+        backgroundColor: const Color(0xFFEDD9B7),
+      ),
+      body: Center(
+        child: ListView.builder(
+          itemCount: integrantes.length,
+          itemBuilder: (context, index) {
+            final integrante = integrantes[index];
+            return ListTile(
+              title: Text('${integrante["nombre"]} ${integrante["apellido"]}', style: TextStyle(fontSize: 16, color: Colors.white),textAlign: TextAlign.center), 
+              subtitle: Text('Padrón: ${integrante["padron"]}',style: TextStyle(fontSize: 16, color: Colors.white),textAlign: TextAlign.center),
+            );
+          },
+        ),
       ),
     );
   }
